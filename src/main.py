c32c3b18f445411c09224aa9c1c6fc6498ca8d3f
3120005129詹皓翔 <2930255852@qq.com>
2022-09-20 13:15:23 +0800

    feat(main): 添加注释

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from textsiml import TextSiml
+import textsiml
 
+# 入口
 if __name__ == "__main__":
     print("Getting Started!\n")
-    result = TextSiml()
-    result.start()
+    textsiml.TextSiml().start()  # 调用模块
     print("\nProgram execution completed!")
