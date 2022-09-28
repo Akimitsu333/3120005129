883e069bf81542bbb3f9ede69e7ce36a0939f697
akimitsu <akimitsu@akimitsu.top>
2022-09-28 18:28:59 +0800

    feat(main): 完成公式提取&转化

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import argparse
 import re
-
-from btree import *
-from stack import *
+from fractions import Fraction
 
 
 def get_args():
@@ -22,17 +20,42 @@ def generate():
     pass
 
 
-def get_formula(path):
-    dict = {}
-    with open(path, "r", encoding="UTF-8") as f:
-        for num, line in enumerate(f):
-            dict[num] = re.match(r"[0-9]+\.\s(.*)\s?", line).group(1)
-    return dict
+def get_formula(file_path, callback_func):
+    resault_dict = {}
+    with open(file_path, "r", encoding="UTF-8") as f:
+        for line_number, line in enumerate(f):
+            formula = re.match(r"[0-9]+\.\s(.*)\s?", line).group(1)
+
+            mixed_number_pattern = re.compile(r"([0-9]+)\'([0-9]+).([0-9]+)")
+            mixed_numbers = mixed_number_pattern.findall(formula)
+            if mixed_numbers:
+                for num in mixed_numbers:
+                    numerator = int(num[0]) * int(num[2]) + int(num[1])
+                    improper_fraction = (
+                        "Fraction(" + str(numerator) + "," + num[2] + ")"
+                    )
+                    formula = mixed_number_pattern.sub(improper_fraction, formula, 1)
+
+            proper_fraction_match = re.compile(r"([0-9]+)/([0-9]+)")
+            proper_fraction = proper_fraction_match.findall(formula)
+            if proper_fraction:
+                for num in proper_fraction:
+                    improper_fraction = "Fraction(" + num[0] + "," + num[1] + ")"
+                    formula = proper_fraction_match.sub(improper_fraction, formula, 1)
+
+            # number = re.sub(
+            #     r"[0-9]+\'[0-9]+/[0-9]+",
+            #     numerator + "/" + number_3,
+            #     real_number,
+            # )
+            # resault_dict[num] = number
+            resault_dict[line_number] = formula
+
+    return resault_dict
 
 
-def build_tree(formula):
-    t_stack = Stack()
-    curent_tree = Node()
+def caculate(formula):
+    result = eval(formula)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,15 +1,41 @@
 import re
+from fractions import Fraction
 
 
-def get_formula(path):
-    dict = {}
-    with open(path, "r", encoding="UTF-8") as f:
-        for num, line in enumerate(f):
-            real_number = re.match(r"[0-9]+\.\s(.*)\s?", line).group(1)
-            easy_number = re.match(r"\s?([0-9]+)\'([0-9]+)\\([0-9]+)\s?", real_number)
-            
-    return dict
+def get_formula(file_path, callback_func):
+    resault_dict = {}
+    with open(file_path, "r", encoding="UTF-8") as f:
+        for line_number, line in enumerate(f):
+            formula = re.match(r"[0-9]+\.\s(.*)\s?", line).group(1)
+
+            mixed_number_pattern = re.compile(r"([0-9]+)\'([0-9]+).([0-9]+)")
+            mixed_numbers = mixed_number_pattern.findall(formula)
+            if mixed_numbers:
+                for num in mixed_numbers:
+                    numerator = int(num[0]) * int(num[2]) + int(num[1])
+                    improper_fraction = (
+                        "Fraction(" + str(numerator) + "," + num[2] + ")"
+                    )
+                    formula = mixed_number_pattern.sub(improper_fraction, formula, 1)
+
+            proper_fraction_match = re.compile(r"([0-9]+)/([0-9]+)")
+            proper_fraction = proper_fraction_match.findall(formula)
+            if proper_fraction:
+                for num in proper_fraction:
+                    improper_fraction = "Fraction(" + num[0] + "," + num[1] + ")"
+                    formula = proper_fraction_match.sub(improper_fraction, formula, 1)
+
+            # number = re.sub(
+            #     r"[0-9]+\'[0-9]+/[0-9]+",
+            #     numerator + "/" + number_3,
+            #     real_number,
+            # )
+            # resault_dict[num] = number
+            resault_dict[line_number] = formula
+
+    return resault_dict
 
 
-dict = get_formula("Exercises.txt")
-print(dict)
+resault_dict = get_formula("Exercises.txt", 1)
+for key, formula in resault_dict.items():
+    print(eval(formula))
