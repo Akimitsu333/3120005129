75caf6526760c421e4114f584464b96875e598c6
akimitsu <akimitsu@akimitsu.top>
2022-09-28 13:37:44 +0800

    feat(btree.py, stack.py): 创建堆栈类、B树类

---
+++ btree.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+    def insertLeft(self, value):
+        if self.left is None:
+            self.left = Node(value)
+        else:
+            node = Node(value)
+            node.left = self.left
+            self.left = node
+
+    def insertRight(self, value):
+        if self.right is None:
+            self.right = Node(value)
+        else:
+            node = Node(value)
+            node.right = self.right
+            self.right = node
